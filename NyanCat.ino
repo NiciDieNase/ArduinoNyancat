1ee96b09f0ade54584424c5f52461d2210f120a8
NiciDieNase <mail@nicidienase.de>
2012-07-27 21:02:43 +0200

    deleted old aruino-projectfile and renamed to NyanCat

--- NyanCat.ino
+++ NyanCat.ino
@@ -82,7 +82,7 @@ void setup() {
     // to calculate the note duration, take one second 
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1500/noteDurations[thisNote];
+    int noteDuration = 1000/noteDurations[thisNote];
     tone(8, intro[thisNote],noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
